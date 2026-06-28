dca496fad7520bda47871a03fcd9bc30c8b933a4
Purushotham <yallurupurushothamreddy006@gmail.com>
2026-06-28 13:44:29 +0530

    Moved from wails to bubbletea GUI

--- client/main.go
+++ client/main.go
@@ -1,36 +1,18 @@
 package main
 
 import (
-	"embed"
+	"fmt"
+	"os"
+	authUI "tanoclient/command/auth"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	tea "charm.land/bubbletea/v2"
 )
 
-//go:embed all:frontend/dist
-var assets embed.FS
-
 func main() {
-	// Create an instance of the app structure
-	app := NewApp()
-
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "tanoclient",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
-
-	if err != nil {
-		println("Error:", err.Error())
+	// client := auth.InitializeNewAPIClient()
+	p := tea.NewProgram(authUI.InitializeAuthIntro())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
 }
