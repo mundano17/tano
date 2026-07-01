b371e13e9f4700e806532bd75dea0c100926daad
Purushotham <yallurupurushothamreddy006@gmail.com>
2026-07-02 00:59:13 +0530

    fix: some controllers now return JSON
    They now return JSON over text for errors to provide a more simplified
    client side dev.

--- server/internal/auth/controllers.go
+++ server/internal/auth/controllers.go
@@ -27,6 +27,10 @@ type tokenResponseBody struct {
 	RefreshToken string `json:"refresh_token"`
 }
 
+type errBody struct {
+	Error string `json:"error"`
+}
+
 func NewAuthController(service Service) *AuthController {
 	return &(AuthController{service: service})
 }
@@ -41,11 +45,23 @@ func (c *AuthController) RegisterController(w http.ResponseWriter, r *http.Reque
 	}
 	err = c.service.RegisterUserService(ctx, creds.Email, creds.Password)
 	if err != nil && errors.Is(err, ErrBadPassword) {
-		http.Error(w, "bad password", http.StatusBadRequest)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "bad password"}
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	if err != nil {
-		http.Error(w, "Server Error", http.StatusInternalServerError)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "server error"}
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -56,16 +72,34 @@ func (c *AuthController) LoginController(w http.ResponseWriter, r *http.Request)
 	creds := new(credentials)
 	err := json.NewDecoder(r.Body).Decode(creds)
 	if err != nil {
-		http.Error(w, "Invalid Body", http.StatusBadRequest)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "invalid body"}
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	accTokenString, refreshTokenString, err := c.service.LoginUserService(ctx, creds.Email, creds.Password)
 	if err != nil && !errors.Is(err, ErrInvalidCredentials) {
-		http.Error(w, "Server Error", 500)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "server error"}
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	if err != nil && errors.Is(err, ErrInvalidCredentials) {
-		http.Error(w, "Invalid Credentials", http.StatusUnauthorized)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "invalid credentials"}
+		w.WriteHeader(http.StatusUnauthorized)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	res := tokenResponseBody{AccessToken: accTokenString, RefreshToken: refreshTokenString}
@@ -81,17 +115,35 @@ func (c *AuthController) LogoutController(w http.ResponseWriter, r *http.Request
 	ctx := r.Context()
 	userIDstr, ok := ctx.Value(UserIDKey).(string)
 	if !ok {
-		http.Error(w, "Invalid Body", http.StatusBadRequest)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "invalid body"}
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	userID, err := strconv.ParseInt(userIDstr, 10, 64)
 	if err != nil {
-		http.Error(w, "Invalid Body", http.StatusBadRequest)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "invalid body"}
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	err = c.service.LogoutService(ctx, userID)
 	if errors.Is(err, ErrUnauthorized) {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		w.Header().Set(
+			"Content-Type",
+			"application/json",
+		)
+		errBody := errBody{Error: "invalid credentials"}
+		w.WriteHeader(http.StatusUnauthorized)
+		_ = json.NewEncoder(w).Encode(errBody)
 		return
 	}
 	w.WriteHeader(http.StatusAccepted)
